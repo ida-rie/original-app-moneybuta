e60152536f031cf3cd194c874195b0117afbeb11
IDA RIE <hrmi9417@gmail.com>
2025-06-14 11:33:34 +0900

    修正

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-// import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: PrismaClient | undefined;
@@ -12,17 +11,3 @@ export const prisma =
 	});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-// Data Proxy 対応の PrismaClient を生成する関数
-// const createPrisma = () =>
-// 	new PrismaClient({
-// 		log: ['query', 'info', 'warn', 'error'],
-// 	}).$extends(withAccelerate());
-
-// グローバルキャッシュ用に型を絞った globalThis ラッパー
-// type PrismaSingleton = { prisma?: ReturnType<typeof createPrisma> };
-// const glb = globalThis as PrismaSingleton;
-
-// development ではキャッシュを使い、production では常に新規生成
-// export const prisma =
-// 	process.env.NODE_ENV === 'production' ? createPrisma() : (glb.prisma ??= createPrisma());
